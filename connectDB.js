4b2cf508cd0832f7cdc5648ccbf3337dcbd33b89
luongxuan11 <xluong2322001@gmail.com>
2024-02-24 05:34:58 +0700

    deploy render

--- connectDB.js
+++ connectDB.js
@@ -1,17 +1,24 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-
-const sequelize = new Sequelize('tiktok', 'root', null, {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+   host: process.env.DB_HOST,
+   dialect: process.env.DB_DIALECT,
+   logging: false,
+   dialectOptions: {
+      ssl: {
+         require: true,
+         rejectUnauthorized: false,
+      },
+   },
 });
-const connection = async () =>{  // function test
-    try {
-        await sequelize.authenticate();
-        console.log('ket noi thanh cong.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
-connection()  
+const connection = async () => {
+   // function test
+   try {
+      await sequelize.authenticate();
+      console.log("ket noi thanh cong.");
+   } catch (error) {
+      console.error("Unable to connect to the database:", error);
+   }
+};
+connection();
