9aa84a6ce215c41d9f9411ffb430c7d03a072286
luongxuan11 <xluong2322001@gmail.com>
2024-02-26 05:57:12 +0700

    connect db

--- connectDB.js
+++ connectDB.js
@@ -1,22 +1,22 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-//    host: process.env.DB_HOST,
-//    dialect: process.env.DB_DIALECT,
-//    logging: false,
-//    dialectOptions: {
-//       ssl: {
-//          require: true,
-//          rejectUnauthorized: false,
-//       },
-//    },
-// });
-const sequelize = new Sequelize("tiktok", "root", null, {
-   host: "localhost",
-   dialect: "mysql",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+   host: process.env.DB_HOST,
+   dialect: process.env.DB_DIALECT,
    logging: false,
+   dialectOptions: {
+      ssl: {
+         require: true,
+         rejectUnauthorized: false,
+      },
+   },
 });
+// const sequelize = new Sequelize("tiktok", "root", null, {
+//    host: "localhost",
+//    dialect: "mysql",
+//    logging: false,
+// });
 const connection = async () => {
    // function test
    try {
